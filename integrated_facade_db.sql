19499fa375dd9f48ead198201ffda41db2a30fa2
Walter <walterg@etraining.edu.co>
2020-02-12 16:38:07 -0500

    Show state categories

--- integrated_facade_db.sql
+++ integrated_facade_db.sql
@@ -7,10 +7,13 @@
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.1.30
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT
+= 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,52 +22,73 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `integrated_facade_db`
+-- Base de datos: integrated_facade_db
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `category`
+-- Estructura de tabla para la tabla category
 --
 
-CREATE TABLE `category` (
-  `id_category` int(100) DEFAULT NULL,
-  `id` int(100) DEFAULT NULL,
-  `name` text DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `parent` int(100) DEFAULT NULL,
-  `visible` int(100) DEFAULT NULL,
-  `timemodified` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE category
+(
+  id_category int(100) DEFAULT NULL,
+  id int(100) DEFAULT NULL,
+  name text DEFAULT NULL,
+  description text DEFAULT NULL,
+  parent int(100) DEFAULT NULL,
+  visible int(100) DEFAULT NULL,
+  timemodified date DEFAULT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `category`
+-- Volcado de datos para la tabla category
 --
 
-INSERT INTO `category` (`id_category`, `id`, `name`, `description`, `parent`, `visible`, `timemodified`) VALUES
-(1, 1, 'Robotica', 'Test', 0, 1, '2020-02-12');
+INSERT INTO category
+  (id_category, id, name, description, parent, visible, timemodified)
+VALUES
+  (1, 1, 'Robotica', 'Test', 0, 1, '2020-02-12');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Estructura de tabla para la tabla user
 --
 
-CREATE TABLE `user` (
-  `Id` int(100) DEFAULT NULL,
-  `Name` varchar(100) DEFAULT NULL,
-  `Pass` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE user
+(
+  Id int(100) DEFAULT NULL,
+  Name varchar(100) DEFAULT NULL,
+  Pass varchar(100) DEFAULT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `user`
+-- Volcado de datos para la tabla user
 --
 
-INSERT INTO `user` (`Id`, `Name`, `Pass`) VALUES
-(1, 'admin', 'MTIzNA==');
+INSERT INTO user
+  (Id, Name, Pass)
+VALUES
+  (1, 'admin', 'MTIzNA==');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+drop table category;
+CREATE TABLE category
+(
+  id_category int(100) DEFAULT NULL,
+  id int(100) DEFAULT NULL,
+  name text DEFAULT NULL,
+  description text DEFAULT NULL,
+  parent int(100) DEFAULT NULL,
+  visible int(100) DEFAULT NULL,
+  timemodified text DEFAULT NULL
+)
+ENGINE=InnoDB DEFAULT CHARSET=latin1
