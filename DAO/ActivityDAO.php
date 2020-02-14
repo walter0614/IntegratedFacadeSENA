ed75aa84bb4662a9c05c4fe0f89d506d981dc059
juankot <juan.kot@hotmail.com>
2020-02-14 16:26:14 -0500

    Commit parcial Activity
    Commit parcial de Actividad para que Daniel apoye con el desarrollo.

---
+++ DAO/ActivityDAO.php
@@ -0,0 +1,29 @@
+<?php
+include_once('../Utils/Utils.php');
+
+class ActivityDAO
+{
+    public static $COURSE_TABLE_NAME = "Activity";
+    public static $ID_COLUMN = "id";
+    public static $ID_ACTIVITY_COLUMN = "id_activity";
+    public static $NAME_COLUMN = "name";
+    public static $COURSE_ID_COLUMN = "courseid";
+    public static $MODULE_ID_COLUMN = "moduleid";
+
+    public static function GetActivitiesByCourseAndModule($conn, $courseId, $moduleId)
+    {
+        $columns = array(
+            ActivityDAO::$ID_COLUMN,
+            ActivityDAO::$ID_ACTIVITY_COLUMN,
+            ActivityDAO::$NAME_COLUMN,
+            ActivityDAO::$COURSE_ID_COLUMN,
+            ActivityDAO::$MODULE_ID_COLUMN
+        );
+        $query = "SELECT " . toString($columns)
+            . " FROM " . ActivityDAO::$COURSE_TABLE_NAME
+            . " WHERE " . ActivityDAO::$COURSE_ID_COLUMN . "=?"
+            . " AND " . ActivityDAO::$MODULE_ID_COLUMN . "=?";
+        $data = $conn->Query($query, array($courseId, $moduleId));
+        return $data;
+    }
+}
--- integrated_facade_db.sql
+++ integrated_facade_db.sql
@@ -91,7 +91,7 @@ CREATE TABLE category
   visible int(100) DEFAULT NULL,
   timemodified text DEFAULT NULL
 )
-ENGINE=InnoDB DEFAULT CHARSET=latin1
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE course
 (
@@ -115,3 +115,16 @@ INSERT INTO course
 VALUES
   (1, 2, 'Prueba 1', 2, '1581462000', '1612998000', '1581461766', '1581461766');
 COMMIT;
+
+-- DROP TABLE activity;
+CREATE TABLE activity
+(
+  id_activity int(100) NOT NULL AUTO_INCREMENT,
+  id int(100) NOT NULL,
+  name text NOT NULL,
+  courseid int(100) NOT NULL,
+  moduleid int(100) NOT NULL,
+  CONSTRAINT activity_pk PRIMARY KEY (id_activity)
+)
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--INSERT INTO activity (id, name, courseid, moduleid) VALUES (11, 'Nombres de animales', 2, 7);
